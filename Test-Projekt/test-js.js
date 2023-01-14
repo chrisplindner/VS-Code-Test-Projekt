3b17425dbccb2fce6510555af0d5e4b5c417b4c6
chris <chrispylind@gmail.com>
2023-01-14 16:37:25 +0100

    Commit 2

--- Test-Projekt/test-js.js
+++ Test-Projekt/test-js.js
@@ -9,14 +9,23 @@ class Person {
     return `${this.first} $${this.last}`;
   }
   set fullName(newName){
-    const [first, last] = newName.split(" "); //neu eigeg. Name wird zu Array gesplitet; Einträge heißten first und last  
-    this.first = first;   //Einträge des Arrays werden als neuer Value der Property zugewiesen; 
+    
+    if(!newName){
+      throw "Problem: Name cannot be blank!!" 
+    }                                  
+    //Fehlermeldung, wenn eingeg. Name leer
+    // nach „throw" wird kein Code weiter ausgeführt
+
+    const [first, last] = newName.split(" ");
+    //neu eingeg. Name wird zu Array gesplitet; Einträge heißen first und last  
+    this.first = first; 
     this.last = last;
+    //Einträge des Arrays werden als neuer Value der Property zugewiesen;
   }
 }
 
 const actor = new Person (Will, Smith);
 
-console.log(actor.fullName());
+console.log(actor.fullName);
 
-
+//K: “Will Smith” Achtung: fullName wird jetzt ohne Klammern aufgerufen
