d780053ecfa67eac523080847781b6deb821adba
chris <chrispylind@gmail.com>
2023-01-23 09:09:11 +0100

    current status 2023-01-23

--- Test-Projekt/test-js.js
+++ Test-Projekt/test-js.js
@@ -1,31 +1,23 @@
 "use strict";
 
-class Person {
-  constructor(first, last) {
-    this.first = first;
-    this.last = last;
+// Only change code below this line
+class Thermostat {
+  constructor(fahrTemp) {
+    this.fahrTemp = fahrTemp;
   }
-  get fullName() {
-    return `${this.first} $${this.last}`;
+  get temperature() {
+    return 5/9*(this.fahrTemp-32);
   }
-  set fullName(newName){
-    
-    if(!newName){
-      throw "Problem: Name cannot be blank!!" 
-    }                                  
-    //Fehlermeldung, wenn eingeg. Name leer
-    // nach „throw" wird kein Code weiter ausgeführt
-
-    const [first, last] = newName.split(" ");
-    //neu eingeg. Name wird zu Array gesplitet; Einträge heißen first und last  
-    this.first = first; 
-    this.last = last;
-    //Einträge des Arrays werden als neuer Value der Property zugewiesen;
+  set temperature(updatedCelsTemp) {
+    this.fahrTemp = updatedCelsTemp * 9.0 / 5 + 32;
   }
 }
+// Only change code above this line
 
-const actor = new Person (Will, Smith);
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
 
-console.log(actor.fullName);
-
-//K: “Will Smith” Achtung: fullName wird jetzt ohne Klammern aufgerufen
+console.log(temp);
+console.log(thermos.fahrTemp);  
